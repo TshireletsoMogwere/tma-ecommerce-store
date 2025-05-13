6cecd589ebb1cce5689951e3ee969b18590b3b96
Tshireletso Mogwere <tmogwere09@gmail.com>
2025-05-13 12:20:03 +0200

    Initialize React with Vite setup & adds Tailwind CSS via CDN for styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+ 
+
+  return (
+    <div className='text-center font-bold text-lg mt-10'>
+      <h1>E-commerce Store</h1>
+    </div>
+  )
+}
+
+export default App
