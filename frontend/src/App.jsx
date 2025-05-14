0e2f9e0533d4d010c8ad3c03a3918f45021de598
Tshireletso Mogwere <tmogwere09@gmail.com>
2025-05-14 14:10:48 +0200

    Adds product page routing ability which prints hard coded product details on proudct page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductPage from './page/ProductDetails';
 
 function App() {
  
 
   return (
+    <>
     <div className='text-center font-bold text-lg mt-10'>
       <h1>E-commerce Store</h1>
     </div>
+      <Router>
+      <Routes>
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </Router>
+    </>
   )
 }
 
